e59e94fc309bd15547461059b561c9b2c9699261
qin.liu1 <qin.liu1@dmall.com>
2024-06-21 14:19:43 +0800

    增加ViT模型的调优示例

---
+++ machine_learning_courses/ViT/finetune_for_endoscopists/READ_ME.txt
@@ -0,0 +1,3 @@
+ref: https://medium.com/@yanis.labrak/how-to-train-a-custom-vision-transformer-vit-image-classifier-to-help-endoscopists-in-under-5-min-2e7e4110a353
+
+data: https://datasets.simula.no/kvasir/#kvasir-v2
---
+++ machine_learning_courses/ViT/finetune_for_endoscopists/finetune.py
@@ -0,0 +1,43 @@
+import argparse
+
+from hugsvision.dataio.VisionDataset import VisionDataset
+
+from hugsvision.nnet.VisionClassifierTrainer import VisionClassifierTrainer
+from transformers import DeiTFeatureExtractor, DeiTForImageClassification
+
+parser = argparse.ArgumentParser(description='Image classifier')
+parser.add_argument('--name', type=str, default="KVASIR_V2", help='The name of the model')
+parser.add_argument('--imgs', type=str, default="./data/kvasir-dataset/",
+                    help='The directory of the input images')
+parser.add_argument('--output', type=str, default="./out/", help='The output directory of the model')
+parser.add_argument('--epochs', type=int, default=1, help='Number of Epochs')
+args = parser.parse_args()
+
+train, test, id2label, label2id = VisionDataset.fromImageFolder(
+    args.imgs,
+    test_ratio=0.15,
+    balanced=True,
+    augmentation=True,
+)
+
+huggingface_model = "facebook/deit-base-distilled-patch16-224"
+
+trainer = VisionClassifierTrainer(
+    model_name=args.name,
+    train=train,
+    test=test,
+    output_dir=args.output,
+    max_epochs=args.epochs,
+    batch_size=32,
+    lr=2e-5,
+    #fp16=True, only available on GPU
+    model=DeiTForImageClassification.from_pretrained(
+        huggingface_model,
+        num_labels=len(id2label),
+        label2id=label2id,
+        id2label=id2label
+    ),
+    feature_extractor=DeiTFeatureExtractor.from_pretrained(huggingface_model)
+)
+
+ref, hyp = trainer.evaluate_f1_score()
