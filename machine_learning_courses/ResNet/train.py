1e9a674d2656df4b23defd3154935061e52e4bcc
qin.liu1 <qin.liu1@dmall.com>
2024-08-28 22:21:51 +0800

    Resnet能跑的版本

--- machine_learning_courses/ResNet/train.py
+++ machine_learning_courses/ResNet/train.py
@@ -21,7 +21,10 @@ y_test = tf.squeeze(y_test, axis=1)
 print(x.shape, y.shape, x_test.shape, y_test.shape)
 
 train_db = tf.data.Dataset.from_tensor_slices((x, y))
-train_db = train_db.shuffle(1000).map(preprocess).batch(8)
+train_db = train_db.shuffle(10).map(preprocess).batch(8)
+
+test_db = tf.data.Dataset.from_tensor_slices((x, y))
+test_db = test_db.map(preprocess).batch(8)
 
 sample = next(iter(train_db))
 print('sample', sample[0].shape, sample[1].shape, tf.reduce_min(sample[0]), tf.reduce_max(sample[0]))
@@ -33,7 +36,7 @@ def main():
     model.build(input_shape=(None, 32, 32, 3))
     optimizer = Adam(learning_rate=1e-3)
 
-    for epoch in range(50):
+    for epoch in range(5):
         for step, (x,y) in enumerate(train_db):
             with tf.GradientTape() as tape:
                 #[b , 32 ,32, 3] => [b, 100]
@@ -44,25 +47,25 @@ def main():
 
             grads = tape.gradient(loss, model.trainable_variables)
             optimizer.apply_gradients(zip(grads, model.trainable_variables))
+            if step%100 == 0:
+                 print('epcoh:', epoch, 'step:', step, 'loss:', float(loss))
+        #Test
+        total_num = 0
+        total_correct = 0
+        for x, y in test_db:
+            logits = model(x)
+            prob = tf.nn.softmax(logits, axis=1)
+            pred = tf.argmax(prob, axis=1)
+            pred = tf.cast(pred, dtype=tf.int32)
 
+            correct = tf.cast(tf.equel(pred, y), dtype=tf.int32)
+            correct = tf.reduce_sum(correct)
 
-    #Test
-    total_num = 0
-    total_correct = 0
-    for x, y in train_db:
-        logits = model(x)
-        prob = tf.nn.softmax(logits, axis=1)
-        pred = tf.argmax(prob, axis=1)
-        pred = tf.cast(pred, dtype=tf.int32)
+            total_num += x.shape[0]
+            total_correct += int(correct)
 
-        correct = tf.cast(tf.equel(pred, y), dtype=tf.int32)
-        correct = tf.reduce_sum(correct)
-
-        total_num += x.shape[0]
-        total_correct += int(correct)
-
-    acc = total_correct / total_num
-    print(epoch, 'acc:', acc)
+        acc = total_correct / total_num
+        print(epoch, 'acc:', acc)
 
 if __name__ == '__main__':
     main()
